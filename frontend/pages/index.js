24926d8d1d2d5a3e330d7022b5955cb87672f291
Jesse Jonas <jessejonas13@gmail.com>
2024-07-30 10:03:57 -0300

    feat: initialized actions

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,46 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
 export default function HomeScreen() {
+  const router = useRouter();
+
+  const [values, setValues] = useState({
+    user: 'jessejonass',
+    password: 'safepassword'
+  });
+
+  const handleChange = (e) => {
+    const fieldValue = e.target.value;
+    const fieldName = e.target.name;
+    
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue
+      }
+    })
+  }
+  
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        router.push('/auth-page-ssr');
+      }}>
         <input
-          placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          placeholder="Usuário" name="user"
+          value={values.user}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          placeholder="Senha" name="password" type="password"
+          value={values.password}
+          onChange={handleChange}
         />
+
+        {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+
         <div>
           <button>
             Entrar
